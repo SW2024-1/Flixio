eae851ae92da56e46081d46089144258d6e2edea
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2024-11-27 07:17:17 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,31 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get "buyitems/new"
-  get "buyitems/create"
-  get "buyitems/destroy"
-    resources :users
-    root 'top#welcome'
-    get 'top/main', to: 'top#main'
-    get 'top/welcome', to: 'top#welcome' 
-    post 'top/login', to: 'top#login', as: 'top_login'
-    get 'top/logout', to: 'top#logout', as: 'top_logout'
-    get 'search', to: 'search#search'
-    get 'search/results', to: 'search#results', as: 'search_results'
-    get 'get_video/:id', to: 'products#get_video', as: 'product_video'
-    get "lists/show"
-    resources :products
-    resources :listitems, only: [:new, :create, :destroy]
-    resources :lists, only: [:show]
-    resources :buyitems, only: [:new, :create, :destroy]  # 新規作成と削除を扱う
-    get 'buys', to: 'buys#show', as: 'buys'
-=======
+   get "buyitems/new"
+   get "buyitems/create"
+   get "buyitems/destroy"
+   
    root 'top#welcome'
    get 'top/main', to: 'top#main'
    get 'top/welcome', to: 'top#welcome' 
    post 'top/login', to: 'top#login', as: 'top_login'
    get 'top/logout', to: 'top#logout', as: 'top_logout'
    
+   get 'search', to: 'search#search'
+   get 'search/results', to: 'search#results', as: 'search_results'
+   
+   get 'get_video/:id', to: 'products#get_video', as: 'product_video'
+   
+   get "lists/show"
+   
+   resources :listitems, only: [:new, :create, :destroy]
+   resources :lists, only: [:show]
+   resources :buyitems, only: [:new, :create, :destroy]  # 新規作成と削除を扱う
+   get 'buys', to: 'buys#show', as: 'buys'
+   
    resources :users do
-     get 'watch_history', on: :member
+   get 'watch_history', on: :member
    end
    
    resources :products do
-     get 'watch_video', on: :member
+   get 'watch_video', on: :member
    end
-   
-   
-   
-   get 'search', to: 'search#search'
-   get 'search/results', to: 'search#results', as: 'search_results'
-   get 'get_video/:id', to: 'products#get_video', as: 'product_video'
-
->>>>>>> watchhistory
 end
