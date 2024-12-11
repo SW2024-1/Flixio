ced3a5bdf8313fbef7930b31f5a17904bfc39740
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2024-12-11 09:01:48 +0000

    動画入れ込み2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,33 @@
 # サンプル動画1
 product1 = Product.create(
   name: "サンプル動画1",
-  price: 100,
-  file: Rails.root.join("public/videos/ex1_kamo.mp4").open
+  price: 100
 )
+# 動画ファイルをbinaryとして保存
+product1.update(file: File.open(Rails.root.join("public/videos/ex1_kamo.mp4")).read)
+
 # サムネイル画像を添付
 product1.thumbnail.attach(io: File.open(Rails.root.join("public/thumbnails/hero.png")), filename: "thumbnail1.png", content_type: "image/png")
 
 # サンプル動画2
 product2 = Product.create(
   name: "サンプル動画2",
-  price: 200,
-  file: Rails.root.join("public/videos/ex2_chabatake.mp4").open
+  price: 200
 )
+# 動画ファイルをbinaryとして保存
+product2.update(file: File.open(Rails.root.join("public/videos/ex2_chabatake.mp4")).read)
+
 # サムネイル画像を添付
 product2.thumbnail.attach(io: File.open(Rails.root.join("public/thumbnails/kaisou.png")), filename: "thumbnail2.png", content_type: "image/png")
+
+# サンプル動画3
+product3 = Product.create(
+  name: "サンプル動画3",
+  price: 300
+)
+# 動画ファイルをbinaryとして保存
+product3.update(file: File.open(Rails.root.join("public/videos/ex1_kamo.mp4")).read)
+
+# サムネイル画像を添付
+product3.thumbnail.attach(io: File.open(Rails.root.join("public/thumbnails/kaisou.png")), filename: "thumbnail2.png", content_type: "image/png")
+
