f58cad2364d1db4d008ea6c121dea8d1004f9853
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2024-12-11 08:34:10 +0000

    動画入れ込み

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,25 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_065206) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "buyitems", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_buyitems_on_list_id"
+    t.index ["product_id"], name: "index_buyitems_on_product_id"
+  end
+
+  create_table "buys", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "user_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_buys_on_product_id"
+    t.index ["user_id"], name: "index_buys_on_user_id"
+  end
+
   create_table "listitems", force: :cascade do |t|
     t.integer "list_id"
     t.integer "product_id", null: false
@@ -66,9 +85,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_065206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "buyitems", "lists"
+  add_foreign_key "buyitems", "products"
+  add_foreign_key "buys", "products"
+  add_foreign_key "buys", "users"
   add_foreign_key "listitems", "products"
 end
