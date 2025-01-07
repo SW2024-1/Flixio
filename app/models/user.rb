291f20cfa194f16533b39713770032626f2020f5
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2025-01-07 11:38:35 +0000

    userのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_one :list
+  has_secure_password
+  has_one :list
+    
+  #バリデーション
+  validates :uid, presence: true  #空ではない
+  validates :password, presence: true  #空ではない
+  validates :uid, length: {maximum: 20 } #20文字以下
+  validates :password, length: {maximum: 20 } #20文字以下
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '正常ケース' do
+    u = User.new
+    u.uid = '正常'
+    u.password = 'seijou'
+    assert u.valid?
+  end
+  
+  test '異常ケース1' do
+    u = User.new
+    assert u.invalid?
+    u.uid = 'A' * 30
+    assert_not u.valid?
+    assert u.errors.include?(:uid)
+  end
+  
+  test '異常ケース2' do
+    u = User.new
+    assert u.invalid?
+    u.uid = ''
+    assert_not u.valid?
+    assert u.errors.include?(:uid)
+  end
+  
+  test '異常ケース3' do
+    u = User.new
+    assert u.invalid?
+    u.password = 'A' * 30
+    assert_not u.valid?
+    assert u.errors.include?(:password)
+  end
+  
+  test '異常ケース4' do
+    u = User.new
+    assert u.invalid?
+    u.password = ''
+    assert_not u.valid?
+    assert u.errors.include?(:password)
+  end
 end
