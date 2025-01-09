1b5e5d293435dcfd3791691870eee586a2e199b5
Kurashige <2211140068a@ed.fuk.kindai.ac.jp>
2025-01-09 08:04:27 +0000

    search修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,30 +2,11 @@ class SearchController < ApplicationController
     
   def search
     @range = params[:range]
-    @search = params[:search]
-    @word = params[:word]
-    if search_params_invalid?
-      flash[:error] = "Invalid search parameters. Please try again."
-      redirect_to root_path and return
-    end
     @products = Product.looks(params[:search], params[:word])
-
   end
   
   def results
     @range = params[:range]
-    @search = params[:search]
-    @word = params[:word]
-    if search_params_invalid?
-      flash[:error] = "Invalid search parameters. Please try again."
-      redirect_to root_path and return
-    end
     @products = Product.looks(params[:search], params[:word])
-  end
-  private
-
-  def search_params_invalid?
-    # search と word が空でないことを確認
-    (@search.blank? || @word.blank?) || !['name', 'description', 'category'].include?(@search)
   end
 end
