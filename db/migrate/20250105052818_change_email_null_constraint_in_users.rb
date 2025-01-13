dc051d90ef04b3047e932a41b6b6ecdb3191e824
Kurashige <2211140068a@ed.fuk.kindai.ac.jp>
2025-01-13 05:05:15 +0000

    修正4

--- db/migrate/20250105052818_change_email_null_constraint_in_users.rb
+++ db/migrate/20250105052818_change_email_null_constraint_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeEmailNullConstraintInUsers < ActiveRecord::Migration[7.2]
   def change
-    change_column :users, :string, null: true
+    change_column_null :users, :email, true
   end
 end
