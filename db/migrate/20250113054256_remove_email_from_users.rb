13a5d8313f5c3e5a2de897627211bf407e8eada0
Ubuntu <ubuntu@ip-172-31-89-166.ec2.internal>
2025-01-14 05:41:40 +0000

    push to render.com

--- db/migrate/20250113054256_remove_email_from_users.rb
+++ db/migrate/20250113054256_remove_email_from_users.rb
@@ -1,8 +1,8 @@
 class RemoveEmailFromUsers < ActiveRecord::Migration[7.2]
   def change
     # emailカラムが存在する場合のみ削除
-    if column_exists?(:users, :email)
-      remove_column :users, :email, :string
-    end
+    #if column_exists?(:users, :email)
+      #remove_column :users, :email, :string
+    #end
   end
 end
